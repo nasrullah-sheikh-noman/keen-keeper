17879667bafde759952a1e6dfd87b73f577c83c2
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-04-14 12:44:58 +0600

    edit some code

---
+++ src/components/homepage/Yourfriends.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Yourfriends = () => {
+  return (
+    <div className='container mx-auto p-4'>
+      <h2 className='text-2xl font-semibold'>Your Friends</h2>
+    </div>
+  );
+};
+
+export default Yourfriends;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Homepagetitle from '../../components/homepage/Homepagetitle';
 import Yourfriendscount from '../../components/homepage/Yourfriendscount';
+import Yourfriends from '../../components/homepage/Yourfriends';
 
 const Home = () => {
   return (
     <div>
       <Homepagetitle/>
       <Yourfriendscount/>
+      <Yourfriends/>
     </div>
   );
 };
